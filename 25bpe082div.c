dd464b771a1f7153bdab6eff6cc8a9d1bfc941d5
anurakti16 <25bpe082@spt.pdpu.ac.in>
2025-10-10 10:40:52 +0530

    Add files via upload

---
+++ 25bpe082div.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void div()
+{
+    int a,b,sum=0;
+    printf("enter two no");
+    scanf("%d%d",&a,&b);
+    sum=a/b;
+    printf("\n sum is =%d",sum);
+
+}
+int main()
+{
+    div();
+}
+
